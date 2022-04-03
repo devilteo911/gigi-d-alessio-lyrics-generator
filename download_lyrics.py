2f2fc172cb67677d89fe84f7b332b50d87b72755
Matteo <matteomoro996@gmail.com>
2022-04-03 21:46:37 +0000

    ✨ added multiple artists download

--- download_lyrics.py
+++ download_lyrics.py
@@ -1,4 +1,4 @@
-# %% 
+# %%
 
 import numpy as np
 import pandas as pd
@@ -10,40 +10,46 @@ from pathlib import Path
 
 # %% building the request and preparing the soup
 
-URL: str = "https://www.lyricsmania.com/gigi_dalessio_lyrics.html"
+THRES = 60
+
+artists: list = ["gigi_dalessio", "nino_dangelo", "pino_daniele"]
 URL_SONGS: str = "https://www.lyricsmania.com"
 # I create a folder to store all the songs
-Path('dataset').mkdir(exist_ok=True)
+Path("dataset").mkdir(exist_ok=True)
 
-artist: str = "gigi_dalessio"
 
 def soupify(url):
     req = requests.get(url)
-    soup = BeautifulSoup(req.content, 'html.parser')
+    soup = BeautifulSoup(req.content, "html.parser")
     return soup
 
-soup = soupify(URL)
-# %% we grab all the artists' song links
-songs_urls: list = []
-for url in soup.find_all("a", title=True):
-    subpage: str = url['href']
-    html_ext = subpage[-5:]
-    # I want to exclude non artists songs and other links
-    if html_ext != ".html" or artist not in subpage:
-        continue
-    else:
-        new_url: str = URL_SONGS + subpage
-        songs_urls.append(new_url)
+
+for artist in artists:
+    URL: str = f"https://www.lyricsmania.com/{artist}_lyrics.html"
+    soup = soupify(URL)
+    # %% we grab all the artists' song links
+    songs_urls: list = []
+    for url in soup.find_all("a", title=True):
+        subpage: str = url["href"]
+        html_ext = subpage[-5:]
+        # I want to exclude non artists songs and other links
+        if html_ext != ".html" or artist not in subpage:
+            continue
+        else:
+            new_url: str = URL_SONGS + subpage
+            songs_urls.append(new_url)
 
 # %% we grab every artists' song text from links
 
 for i, url in tqdm(enumerate(songs_urls), total=len(songs_urls)):
-    if i == 60: break
-    # To avoid IP ban
-    if i % 20 == 0: time.sleep(5)
+    # if i == THRES:
+    #     break
+    # To avoid IP ban - setting a five seconds stop after scraping 20 songs
+    if i % 20 == 0:
+        time.sleep(5)
     # I create the request and parse it with bs4
     soup = soupify(url)
-    for div in soup.find_all("div",  attrs={'class':"lyrics-body"}):       
+    for div in soup.find_all("div", attrs={"class": "lyrics-body"}):
         with open(f"dataset/songs_{i}.csv", "w") as f:
             f.write(div.text)
     # To avoid IP ban
